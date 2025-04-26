511da805e48578aa2bf26a402e6d08465fd58ac9
kclnk <192100140@ecu.edu.eg>
2025-04-26 17:57:26 +0300

    added json logging data, improved Dashboard

--- load_balancer.py
+++ load_balancer.py
@@ -1,11 +1,14 @@
 # load_balancer.py
-from flask import Flask, request, redirect
+from flask import Flask, request, redirect, jsonify
 import itertools
 import threading
 import time
 import requests
+from collections import defaultdict
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app, origins=["http://127.0.0.1:8000"])
 
 servers = [
     "http://127.0.0.1:5001",
@@ -14,6 +17,7 @@ servers = [
 ]
 
 server_status = {server: True for server in servers}
+server_request_count = defaultdict(int)
 server_cycle = itertools.cycle(servers)
 
 def health_check():
@@ -32,8 +36,23 @@ def balance():
     for _ in range(len(servers)):
         target = next(server_cycle)
         if server_status.get(target, False):
+            server_request_count[target] += 1  # Track requests
             return redirect(f"{target}/", code=307)
     return "No backend servers available.", 503
+
+@app.route('/stats', methods=['GET'])
+def stats():
+    data = {
+        "servers": []
+    }
+    for server in servers:
+        data['servers'].append({
+            "url": server,
+            "status": server_status[server],
+            "requests": server_request_count[server]
+        })
+    return jsonify(data)
+
 
 if __name__ == '__main__':
     threading.Thread(target=health_check, daemon=True).start()
